abf4089f7ef312df28b846f04a039d888521b5cc
Neosaru <75719656+Neosaru@users.noreply.github.com>
2022-10-14 19:42:46 +1100

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,4 +1,22 @@
 
 public class Room {
-
+	
+	public static void StartRoom() {
+		//dialogue
+		//actions
+		//what rooms you can go to
+		// inventory
+	}
+	
+	public static void ShopRoom() {
+		
+	}
+	
+	public static void EndRoom() {
+		
+	}
+	
+	public static void POIRoom() {
+		
+	}
 }
